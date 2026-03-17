f083f0383874b3af5161a23872334a41cbb40003
Giovana Santos <giovanasilva.office@gmail.com>
2026-03-16 23:00:24 -0300

    Define LoginScreen como tela inicial na MainActivity

--- app/src/main/java/com/github/Gihxs/navigation_app/MainActivity.kt
+++ app/src/main/java/com/github/Gihxs/navigation_app/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.github.Gihxs.navigation_app.screens.LoginScreen
 import com.github.Gihxs.navigation_app.ui.theme.NavigationappTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NavigationappTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    LoginScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
